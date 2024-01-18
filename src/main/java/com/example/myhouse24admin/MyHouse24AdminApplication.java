3dffb087a7a0b39569febb293742ed3f84fb504b
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-01-18 16:13:03 +0200

    Created project

---
+++ src/main/java/com/example/myhouse24admin/MyHouse24AdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhouse24admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHouse24AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHouse24AdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myhouse24admin/MyHouse24AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhouse24admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHouse24AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
