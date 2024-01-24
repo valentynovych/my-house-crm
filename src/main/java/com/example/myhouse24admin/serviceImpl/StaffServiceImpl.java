e24523a0d325148740076d437a4587478729ea49
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-01-24 12:28:57 +0200

    Security - Login: added logging

--- src/main/java/com/example/myhouse24admin/serviceImpl/StaffServiceImpl.java
+++ src/main/java/com/example/myhouse24admin/serviceImpl/StaffServiceImpl.java
@@ -8,6 +8,8 @@ import com.example.myhouse24admin.repository.RoleRepo;
 import com.example.myhouse24admin.repository.StaffRepo;
 import com.example.myhouse24admin.service.StaffService;
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,7 @@ public class StaffServiceImpl implements StaffService {
     private final RoleRepo roleRepo;
     private final PasswordEncoder passwordEncoder;
     private final StaffMapper staffMapper;
+    private final Logger logger = LogManager.getLogger("serviceLogger");
 
     public StaffServiceImpl(StaffRepo staffRepo, RoleRepo roleRepo, PasswordEncoder passwordEncoder, StaffMapper staffMapper) {
         this.staffRepo = staffRepo;
@@ -27,6 +30,7 @@ public class StaffServiceImpl implements StaffService {
 
     @Override
     public void createFirstStaff() {
+        logger.info("createFirstStaff() - Creating first staff");
         if(isTableEmpty()){
             Role role = roleRepo.findById(1L).orElseThrow(()-> new EntityNotFoundException("Role not found by id 1"));
             Staff staff = staffMapper.createFirstStaff("admin@gmail.com",
@@ -34,6 +38,7 @@ public class StaffServiceImpl implements StaffService {
                     "Директор","+380991111111",role, Language.UKR);
             saveStaffInDB(staff);
         }
+        logger.info("createFirstStaff() - First staff was created");
     }
     private boolean isTableEmpty(){
         return staffRepo.count() == 0;
