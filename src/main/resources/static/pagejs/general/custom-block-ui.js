9fdac84cbdbfa0c5c7a11bdb0504b79ebc333883
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2024-02-04 14:54:06 +0200

    add to custom-block-ui.js function for block .card-body and add function block/unblock by custom className

--- src/main/resources/static/pagejs/general/custom-block-ui.js
+++ src/main/resources/static/pagejs/general/custom-block-ui.js
@@ -1,4 +1,4 @@
-$(document).on('ajaxStart', function () {
+function blockCardDody() {
     $('.card-body').block({
         message: `<div class="col d-flex justify-content-center ">
                  <!-- Wave -->
@@ -20,8 +20,36 @@ $(document).on('ajaxStart', function () {
             opacity: 0.8
         }
     })
-});
+}
 
 $(document).on('ajaxStop', function () {
     $('.card-body').unblock();
 })
+
+function blockClass(className) {
+    $(`.${className}`).block({
+        message: `<div class="col d-flex justify-content-center ">
+                 <!-- Wave -->
+                 <div class="sk-wave sk-primary">
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                 </div>
+               </div>`,
+        css: {
+            backgroundColor: "transparent",
+            border: "0",
+            position: "center"
+        },
+        overlayCSS: {
+            backgroundColor: "#fff",
+            opacity: 0.8
+        }
+    })
+}
+
+function unblockClass(className) {
+    $(`.${className}`).unblock();
+}
