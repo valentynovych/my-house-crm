02075d3e52c2c20ac13b7d2781d63130ff1208f5
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-02-08 22:51:06 +0200

    Logout - added localization

--- src/main/resources/static/pagejs/layout/layout.js
+++ src/main/resources/static/pagejs/layout/layout.js
@@ -47,27 +47,29 @@ function showMenuItems(permissions){
 
 $("#logoutLink").on("click", function (e) {
     e.preventDefault();
-    $("div.card").append(
-        '<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel"\n' +
-        '             aria-hidden="true">\n' +
-        '            <div class="modal-dialog modal-dialog-centered">\n' +
-        '                <div class="modal-content">\n' +
-        '                    <div class="modal-header">\n' +
-        '                        <button type="button" class="btn-close" data-bs-dismiss="modal"\n' +
-        '                                aria-label="Close"></button>\n' +
-        '                    </div>\n' +
-        '                    <div class="modal-body">\n' +
-        '                        <h4>Ви впевнені що хочете вийти?</h4>\n' +
-        '                    </div>\n' +
-        '                    <div class="modal-footer">\n' +
-        '                        <button type="button" class="btn btn-danger" onclick="logout()" id="logoutButton"">\n' +
-        '                            Так\n' +
-        '                        </button>\n' +
-        '                    </div>\n' +
-        '                </div>\n' +
-        '            </div>\n' +
-        '        </div>'
-    )
+    if($('#logoutModal').length === 0) {
+        $("div.card").append(
+            `<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <h4>${logoutModalMessage}</h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" onclick="logout()" id="logoutButton"">
+                            ${logoutModalYes}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>`
+        )
+    }
     $('#logoutModal').modal('show');
 });
 function logout () {
