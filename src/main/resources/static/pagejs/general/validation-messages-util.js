70233e9f772ca5c48375852ade95176bd7e13bac
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2024-02-14 01:17:47 +0200

    validation-messages-util.js - add new condition - not add error message to disabled input

--- src/main/resources/static/pagejs/general/validation-messages-util.js
+++ src/main/resources/static/pagejs/general/validation-messages-util.js
@@ -3,9 +3,15 @@ function printErrorMessageToField(errorResponse) {
     if (errorResponse.status === 400) {
         const errorMap = new Map(Object.entries((errorResponse.responseJSON)));
         errorMap.forEach((value, key) => {
-            $('[name="' + key + '"]').addClass("is-invalid").parent().append($(
-                '<p class="error-message invalid-feedback m-0">' + value + '</p>'
-            ));
+            const input = $('[name="' + key + '"]');
+            if (!input.attr('disabled')) {
+                input.addClass("is-invalid").parent().append($(
+                    '<p class="error-message invalid-feedback m-0">' + value + '</p>'));
+                input.on('change', function () {
+                    $(this).removeClass('is-invalid');
+                    $(this).closest('.invalid-feedback').remove();
+                });
+            }
         })
     }
 }
