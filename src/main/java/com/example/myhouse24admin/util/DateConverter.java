6f7880ff3cce81169714bf4a4dc94bc0a4acea9e
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2024-03-06 02:08:19 +0200

    Utils -> DateConverter - refactor methods | add final static variables

--- src/main/java/com/example/myhouse24admin/util/DateConverter.java
+++ src/main/java/com/example/myhouse24admin/util/DateConverter.java
@@ -4,14 +4,19 @@ import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 public class DateConverter {
-    public static String instantToString(Instant date){
-        LocalDate localDate = LocalDate.ofInstant(date, ZoneId.systemDefault());
-        return localDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    private final static ZoneId ZONE_ID = ZoneId.systemDefault();
+    private final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public static String instantToString(Instant date) {
+        LocalDate localDate = LocalDate.ofInstant(date, ZONE_ID);
+        return localDate.format(DATE_TIME_FORMATTER);
     }
-    public static Instant stringToInstant(String date){
-        LocalDateTime localDateTime = LocalDateTime.of(LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy")),
+
+    public static Instant stringToInstant(String date) {
+        LocalDateTime localDateTime = LocalDateTime.of(LocalDate.parse(date, DATE_TIME_FORMATTER),
                 LocalTime.now());
-        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZONE_ID);
         return zonedDateTime.toInstant();
     }
 }
