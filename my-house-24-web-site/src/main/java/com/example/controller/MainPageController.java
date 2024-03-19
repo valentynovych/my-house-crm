71ac6acba27befe8f7e8e3b48dfbd61820263cf3
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-03-20 01:28:33 +0200

    Web-site - done navbar and footer

---
+++ my-house-24-web-site/src/main/java/com/example/controller/MainPageController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/web-site")
+public class MainPageController {
+    @GetMapping("")
+    public ModelAndView getLoginPage() {
+        return new ModelAndView("layout/layout");
+    }
+}
