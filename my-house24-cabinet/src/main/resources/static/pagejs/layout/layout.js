6326626e4ea98c4d03ab1a19297a4fca2734dc92
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2024-03-19 15:02:14 +0200

    Layout - fix mark active current menu-item

--- my-house24-cabinet/src/main/resources/static/pagejs/layout/layout.js
+++ my-house24-cabinet/src/main/resources/static/pagejs/layout/layout.js
@@ -1,9 +1,7 @@
-$(document).ready(function () {
-    const currHref = window.location.pathname;
-    $('.menu-item a').each(function (i, item) {
-        if (currHref.includes($(item).attr('href'))) {
-            $(item).parent().addClass('active')
-            $(this).parents('[data-parrent]').addClass("active open");
-        }
-    });
+const currHref = window.location.pathname;
+$('.menu-item a').each(function (i, item) {
+    if (currHref === ($(item).attr('href'))) {
+        $(item).parent().addClass('active')
+        $(this).parents('[data-parrent]').addClass("active open");
+    }
 });
